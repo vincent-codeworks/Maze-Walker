0c8972abd68faa273b634ceade0192ed88f23fb3
Vincent BOURGEOIS <DK6071@engie.com>
2025-03-20 14:49:06 +0100

    really added tests

---
+++ CodeAndExamples/MazeSolver/MazeSolverTests/MazeTests.cs
@@ -0,0 +1,34 @@
+﻿using MazeSolver;
+
+namespace MazeSolverTests;
+public class MazeTests
+{
+    [Fact]
+    public void Golden_master()
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        var expectedOutput = """
+            Point(1, 1)
+            Point(2, 1)
+            Point(3, 1)
+            Point(4, 1)
+            Point(4, 1)
+            Point(4, 1)
+            Point(3, 1)
+            Point(2, 1)
+            Point(2, 2)
+            Point(2, 3)
+            Point(3, 3)
+            Point(4, 3)
+            Point(5, 3)
+            Reached end of maze! :)
+            
+            """;
+        var mazeApp = new MazeApp();
+        mazeApp.Run(@"MazeFiles\maze1.txt");
+        var actualOutput = stringWriter.ToString();
+        Assert.Equal(expectedOutput, actualOutput);
+
+    }
+}
