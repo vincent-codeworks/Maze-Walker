886514e68c7cd7fa5c317f44edffa747a2132efd
Vincent BOURGEOIS <DK6071@engie.com>
2025-03-20 15:16:46 +0100

    moved maez extraction into specific class

--- CodeAndExamples/MazeSolver/MazeSolver/MazeApp.cs
+++ CodeAndExamples/MazeSolver/MazeSolver/MazeApp.cs
@@ -18,60 +18,12 @@ public class MazeApp
     public void Run(string mazeFilePath)
     {
         var lines = File.ReadAllLines(mazeFilePath).Select(l => l.Replace(" ", "")).ToArray();
-        MazeGrid maze = ExtractMaze(lines);
+        MazeGrid maze = MazeBuilder.ExtractMaze(lines);
         var entity = new DumbMazeWalker(maze);
         var solved = entity.SolveMaze();
         if (solved)
         {
             Console.WriteLine("Reached end of maze! :)");
         }
-    }
-
-    private static MazeGrid ExtractMaze(string[] lines)
-    {
-        Point start = null;
-        Point finish = null;
-
-        var grid = new bool[lines.Length][];
-        int currentRow = 0;
-
-        foreach (var line in lines)
-        {
-            grid[currentRow] = new bool[line.Length];
-            int currentCol = 0;
-
-            foreach (var point in line)
-            {
-                switch (point)
-                {
-                    case '#':
-                        grid[currentRow][currentCol] = false;
-                        break;
-                    case '.':
-                        grid[currentRow][currentCol] = true;
-                        break;
-                    case 'S':
-                        grid[currentRow][currentCol] = true;
-                        start = new Point(currentCol, currentRow);
-                        break;
-                    case 'F':
-                        grid[currentRow][currentCol] = true;
-                        finish = new Point(currentCol, currentRow);
-                        break;
-                    default:
-                        throw new Exception("Maze input string contains invalid characters");
-                }
-
-                currentCol++;
-            }
-
-            currentRow++;
-        }
-
-        if (start == null) throw new Exception("Maze should have a start position set.");
-        if (finish == null) throw new Exception("Maze should have a finish position set.");
-
-        var maze = new MazeGrid(grid, start, finish);
-        return maze;
     }
 }
